cc02faa7af4b83e3642e1221f7b45bdd86bf71b8
Brian <120551412+DnKhanhh@users.noreply.github.com>
2024-05-14 16:27:37 +0700

    update bottom tabbar navigation

---
+++ App/src/assets/svgs/index.ts
@@ -0,0 +1,23 @@
+import HOME from './root/home.svg';
+import INACTIVE_HOME from './root/home_inactive.svg';
+import SEARCH from './root/search.svg';
+import INACTIVE_SEARCH from './root/search_inactive.svg';
+import NEWS from './root/news.svg';
+import LIKE from './root/like.svg';
+import INACTIVE_LIKE from './root/like_inactive.svg';
+import USER from './root/user.svg';
+import INACTIVE_USER from './root/user_inactive.svg';
+
+const SVG_NAME = {
+  HOME,
+  INACTIVE_HOME,
+  SEARCH,
+  INACTIVE_SEARCH,
+  NEWS,
+  LIKE,
+  INACTIVE_LIKE,
+  USER,
+  INACTIVE_USER,
+};
+
+export default SVG_NAME;
--- App/src/screens/Home/view.tsx
+++ App/src/screens/Home/view.tsx
@@ -1,10 +1,19 @@
 import {Text, View} from 'react-native';
 import React from 'react';
+import SVG_NAME from '@/assets/svgs';
+
+const IconHandle = (iconName: any, focused: boolean) => {
+  let iconNameVal = SVG_NAME[iconName];
+  let iconInactiveNameVal = SVG_NAME[`INACTIVE_${iconName}`];
+  console.log('ICON', {iconNameVal});
+  return focused ? <>{iconNameVal}</> : <>{iconInactiveNameVal}</>;
+};
 
 const HomeScreenView = () => {
   return (
     <View>
       <Text>HomeScreenView</Text>
+      {IconHandle('HOME', true)}
     </View>
   );
 };
